3375f59af43f2c51567943c8ecf7808eb9cf063c
Milos Kleint <mkleint@atlassian.com>
2016-02-04 13:06:07 +1100

    BUILDENG-10188 js script added to turn capability executable <select>s into <input>s

---
+++ bamboo-isolated-docker-plugin/src/main/java/com/atlassian/buildeng/isolated/docker/UIRest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2016 Atlassian.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.atlassian.buildeng.isolated.docker;
+
+import com.atlassian.bamboo.plan.PlanKey;
+import com.atlassian.bamboo.plan.PlanKeys;
+import com.atlassian.bamboo.plan.cache.CachedPlanManager;
+import com.atlassian.bamboo.plan.cache.ImmutableJob;
+import com.atlassian.bamboo.plan.cache.ImmutablePlan;
+import com.atlassian.bamboo.task.TaskDefinition;
+import com.atlassian.bamboo.util.Narrow;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@Path("/ui")
+public class UIRest {
+
+    private final CachedPlanManager cpm;
+
+    public UIRest(CachedPlanManager cpm) {
+        this.cpm = cpm;
+    }
+
+
+    @GET
+    @Path("/enabled")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response isDockerEnabled(@QueryParam("jobKey") String jobKey) {
+        final PlanKey key = PlanKeys.getPlanKey(jobKey);
+        ImmutablePlan job = cpm.getPlanByKey(key);
+        if (job == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Job not found").build();
+        }
+        ImmutableJob jb = Narrow.downTo(job, ImmutableJob.class);
+        if (jb == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Not a Job Key").build();
+        }
+        Configuration conf = Configuration.forJob(jb);
+        JSONObject toRet = new JSONObject();
+        toRet.put("enabled", conf.isEnabled());
+        JSONArray result = new JSONArray();
+        toRet.put("tasks", result);
+        if (conf.isEnabled()) {
+            jb.getTaskDefinitions().stream().forEach((TaskDefinition t) -> {
+                try {
+                    JSONObject obj = new JSONObject();
+                    obj.put("id", t.getId());
+                    obj.put("label", t.getConfiguration().get("label"));
+                    obj.put("buildJdk", t.getConfiguration().get("buildJdk"));
+                    result.put(obj);
+                } catch (JSONException ex) {
+                    Logger.getLogger(UIRest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+        }
+        return Response.ok().entity(toRet.toString()).build();
+    }
+}
---
+++ bamboo-isolated-docker-plugin/src/main/resources/selectToField.js
@@ -0,0 +1,86 @@
+/* 
+ * Copyright 2016 Atlassian.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* global insertionQ, BAMBOO */
+
+AJS.$(document).ready(function() {
+    var path = window.location.pathname;
+    var isJobTaskEdit = path.indexOf("/build/admin/edit/editBuildTasks.action") !== -1;
+    if (isJobTaskEdit) {
+        var indexedData = {};
+        var update = function(cache) {
+            if( $('.task-config').length) {
+                var data = {};
+                if (cache.enabled) { //0 is newly added task.. if docker is enabled, put it here to modify that ui as well.
+                    data[0] = {};
+                    data[0].label = "";
+                    data[0].buildJdk = "";
+                }
+                $.each(cache.tasks, function (index, item) {
+                    data[item.id] = item;
+                });
+                indexedData = data;
+            }
+        };
+        var findTaskIndex = function() {
+            var index = $("form#updateTask input#updateTask_taskId").attr("value");
+            if (! index ) {
+                index = $("form#createTask input#createTask_taskId").attr("value");
+            }
+            return index;
+        }
+        insertionQ('select.builderSelectWidget').every(function (element) {
+            var jq = $(element);
+            var index = findTaskIndex();
+            if (indexedData[index]) {
+                jq.after("<div class='description' id='dockerBuilderDesc'><b>With Isolated docker enabled, this field is freeform. Please enter the value that the docker based agent provides</b></div>");
+                jq.replaceWith("<input type='text' name='" + element.name + "' id='" + element.id + "' class='text long-field' value='" + indexedData[index].label + "'>");
+                $("form a.addSharedBuilderCapability").remove();
+            }
+            return jq;
+        });
+        insertionQ('select.jdkSelectWidget').every(function (element) {
+            var jq = $(element);
+            var index = findTaskIndex();
+            if (indexedData[index]) {
+                jq.after("<div class='description' id='dockerJdkDesc'><b>With Isolated docker enabled, this field is freeform. Please enter the value that the docker based agent provides</b></div>");
+                jq.replaceWith("<input type='text' name='" + element.name + "' id='" + element.id + "' class='text long-field' value='" + indexedData[index].buildJdk + "'>");
+                $("form a.addSharedJdkCapability").remove();
+            }
+            return jq;
+        });
+        var doAjax = function () {
+            AJS.$.ajax({
+                type: 'GET',
+                url: AJS.contextPath() + "/rest/docker-ui/1.0/ui/enabled?jobKey=" + BAMBOO.currentPlan.key,
+                success: function (text) {
+                    update(text);
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    update({});
+                }
+
+            });
+
+        };
+        insertionQ('div#no-item-selected').every(function (element) {
+            doAjax();
+        });
+        doAjax();
+    }
+
+});
+
