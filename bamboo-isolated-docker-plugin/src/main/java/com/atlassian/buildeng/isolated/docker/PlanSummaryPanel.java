123954d6f0de2286b11cde23bce5bfd0ab1314ea
Milos Kleint <mkleint@atlassian.com>
2016-03-08 14:19:27 +1100

    BUILDENG-10358 job summaries list what docker image was used to build given job + error if present and plan summary lists all jobs built with docker agents

---
+++ bamboo-isolated-docker-plugin/src/main/java/com/atlassian/buildeng/isolated/docker/PlanSummaryPanel.java
@@ -0,0 +1,39 @@
+package com.atlassian.buildeng.isolated.docker;
+
+import com.atlassian.bamboo.chains.ChainResultsSummary;
+import com.atlassian.bamboo.resultsummary.BuildResultsSummary;
+import com.atlassian.bamboo.resultsummary.ResultsSummary;
+import com.atlassian.plugin.web.model.WebPanel;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+public class PlanSummaryPanel implements WebPanel {
+    @Override
+    public String getHtml(Map<String, Object> context) {
+        ChainResultsSummary summary = (ChainResultsSummary) context.get("resultSummary");
+        StringBuilder ret = new StringBuilder();
+        for (ResultsSummary brs : summary.getOrderedJobResultSummaries()) {
+            Configuration config = Configuration.forBuildResultSummary((BuildResultsSummary) brs);
+            if (config.isEnabled()) {
+                ret.append("<dt>")
+                   .append(brs.getImmutablePlan().getBuildName())
+                   .append("</dt><dd>")
+                   .append(config.getDockerImage())
+                   .append("</dd>");
+            }
+        }
+        if (ret.length() > 0) {
+            ret.insert(0, "<h2>Jobs built by Isolated Docker</h2><dl class=\"details-list\">");
+            ret.append("</dl>");
+            return ret.toString();
+        }
+        return "";
+    }
+
+    @Override
+    public void writeHtml(Writer writer, Map<String, Object> context) throws IOException {
+        writer.append(getHtml(context));
+    }
+}
