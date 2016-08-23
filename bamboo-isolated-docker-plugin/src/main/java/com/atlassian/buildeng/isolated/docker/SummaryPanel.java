5863b535046b5b54da2aa120df426e3f7be07d50
Milos Kleint <mkleint@atlassian.com>
2016-08-23 10:19:48 +1000

    BUILDENG-11560 when a build is queued, render the page based on the queued result, rather than the previous persisted one.

--- bamboo-isolated-docker-plugin/src/main/java/com/atlassian/buildeng/isolated/docker/SummaryPanel.java
+++ bamboo-isolated-docker-plugin/src/main/java/com/atlassian/buildeng/isolated/docker/SummaryPanel.java
@@ -1,7 +1,10 @@
 package com.atlassian.buildeng.isolated.docker;
 
+import com.atlassian.bamboo.plan.PlanResultKey;
 import com.atlassian.buildeng.spi.isolated.docker.Configuration;
 import com.atlassian.bamboo.resultsummary.BuildResultsSummary;
+import com.atlassian.bamboo.v2.build.BuildContext;
+import com.atlassian.bamboo.v2.build.queue.BuildQueueManager;
 import com.atlassian.plugin.web.model.WebPanel;
 
 import java.io.IOException;
@@ -10,16 +13,29 @@ import java.util.Map;
 
 @SuppressWarnings("UnnecessarilyQualifiedInnerClassAccess")
 public class SummaryPanel implements WebPanel {
+    
+    private final BuildQueueManager buildQueueManager;
+
+    public SummaryPanel(BuildQueueManager buildQueueManager) {
+        this.buildQueueManager = buildQueueManager;
+    }
+    
     @Override
     public String getHtml(Map<String, Object> context) {
         BuildResultsSummary summary = (BuildResultsSummary) context.get("resultSummary");
-        Configuration configuration = Configuration.forBuildResultSummary(summary);
+        Map<PlanResultKey, BuildContext> map = PlanSummaryPanel.mapKeyToBuildContext(buildQueueManager);
+        BuildContext buildcontext = map.get(summary.getPlanResultKey());
+        
+        //when a build is queued, we derive data from the CurrentResult, not the persisted value (reruns)
+        Configuration configuration = buildcontext != null ? Configuration.forContext(buildcontext) : Configuration.forBuildResultSummary(summary);
         StringBuilder ret = new StringBuilder();
         if (configuration.isEnabled()) {
             ret.append("<h2>Built with Isolated Docker</h2>")
                .append("<dl class=\"details-list\"><dt>Image used:</dt><dd>")
                .append(configuration.getDockerImage()).append("</dd>");
-            String error = summary.getCustomBuildData().get(Constants.RESULT_ERROR);
+            String error = buildcontext != null 
+                    ? buildcontext.getCurrentResult().getCustomBuildData().get(Constants.RESULT_ERROR) 
+                    : summary.getCustomBuildData().get(Constants.RESULT_ERROR);
             if (error != null) {
                 ret.append("<dt>Error:</dt><dd class=\"errorText\">")
                    .append(error)
