68136605d5187f6ff82c1a3b1ee4a3bf8a3be746
Milos Kleint <mkleint@atlassian.com>
2016-09-09 13:17:55 +1000

    noissue: remove pods with exited bamboo-agent container.

--- bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesIsolatedDockerImpl.java
+++ bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesIsolatedDockerImpl.java
@@ -20,7 +20,8 @@ import com.atlassian.buildeng.spi.isolated.docker.Configuration;
 import com.atlassian.buildeng.spi.isolated.docker.IsolatedAgentService;
 import com.atlassian.buildeng.spi.isolated.docker.IsolatedDockerAgentRequest;
 import com.atlassian.buildeng.spi.isolated.docker.IsolatedDockerRequestCallback;
-import io.fabric8.kubernetes.api.model.DownwardAPIVolumeSource;
+import com.atlassian.sal.api.lifecycle.LifecycleAware;
+import com.atlassian.sal.api.scheduling.PluginScheduler;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodBuilder;
@@ -28,16 +29,20 @@ import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import java.time.Duration;
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
+import java.util.Map;
 
 /**
  *
  * @author mkleint
  */
-public class KubernetesIsolatedDockerImpl implements IsolatedAgentService {
+public class KubernetesIsolatedDockerImpl implements IsolatedAgentService, LifecycleAware {
     /**
      * The environment variable to override on the agent per image
      */
@@ -52,28 +57,46 @@ public class KubernetesIsolatedDockerImpl implements IsolatedAgentService {
      * The environment variable to set the result spawning up the agent
      */
     static String ENV_VAR_RESULT_ID = "RESULT_ID";
+    private final String PLUGIN_JOB_KEY = "KubernetesIsolatedDockerImpl";
+    private static final long PLUGIN_JOB_INTERVAL_MILLIS = Duration.ofSeconds(30).toMillis();
+    static final String CONTAINER_NAME_BAMBOOAGENT = "bamboo-agent";
+    
+    //configuration
+    static final String KUB_HOST = "https://192.168.99.100:8443";
+    static final String NAMESPACE_BAMBOO = "bamboo";
+
     
     private final AdministrationConfigurationAccessor admConfAccessor;
+    private final PluginScheduler pluginScheduler;
 
-    public KubernetesIsolatedDockerImpl(AdministrationConfigurationAccessor admConfAccessor) {
+    public KubernetesIsolatedDockerImpl(AdministrationConfigurationAccessor admConfAccessor, PluginScheduler pluginScheduler) {
         this.admConfAccessor = admConfAccessor;
+        this.pluginScheduler = pluginScheduler;
     }
 
     @Override
     public void startAgent(IsolatedDockerAgentRequest request, IsolatedDockerRequestCallback callback) {
+        try (KubernetesClient client = createKubernetesClient()) {
+            //TODO only create namespace when creation of pod fails?
+            if (!client.namespaces().list().getItems().stream()
+                    .filter((Namespace t) -> NAMESPACE_BAMBOO.equals(t.getMetadata().getName()))
+                    .findFirst().isPresent()) {
+                System.out.println("no bamboo namespace, creating");
+                client.namespaces().createNew().withNewMetadata().withName(NAMESPACE_BAMBOO).endMetadata().done();
+            }
+            Pod pod = createPod(request);
+            System.out.println("POD:" + pod.toString());
+            pod = client.pods().create(pod);
+            //TODO do we extract useful information from the podstatus here?
+        }
+    }
+
+    static KubernetesClient createKubernetesClient() throws KubernetesClientException {
         Config config = new ConfigBuilder()
-                .withMasterUrl("https://192.168.99.100:8443")
+                .withMasterUrl(KUB_HOST)
                 .build();
         KubernetesClient client = new DefaultKubernetesClient(config);
-        if (!client.namespaces().list().getItems().stream()
-                .filter((Namespace t) -> "bamboo".equals(t.getMetadata().getName()))
-                .findFirst().isPresent()) {
-            System.out.println("no bamboo namespace, creating");
-            client.namespaces().createNew().withNewMetadata().withName("bamboo").endMetadata().done();
-        }
-        Pod pod = createPod(request);
-        System.out.println("POD:" + pod.toString());
-        client.pods().create(pod);
+        return client;
     }
 
     @Override
@@ -85,7 +108,7 @@ public class KubernetesIsolatedDockerImpl implements IsolatedAgentService {
         Configuration c = r.getConfiguration();
         PodBuilder pb = new PodBuilder()
             .withNewMetadata()
-                .withNamespace("bamboo")
+                .withNamespace(NAMESPACE_BAMBOO)
                 .withName(r.getResultKey().toLowerCase(Locale.ENGLISH) + "-" + r.getUniqueIdentifier().toString())
                 .addToLabels("resultId", r.getResultKey())
             .endMetadata()
@@ -96,13 +119,14 @@ public class KubernetesIsolatedDockerImpl implements IsolatedAgentService {
                     //more or less same data as regular sidekick, just extending alpine to be able to 
                     // run shell scripts inside + /kubernetes.sh script to copy to shared directory
                     .withImage("docker.atlassian.io/buildeng/bamboo-agent-sidekick-k8s")
-//                    .withImagePullPolicy("Never")
+                    //kubernetes fails to pull from docker.a.io and this skips the container. ??
+                    .withImagePullPolicy("Never")
                     .addNewVolumeMount("/buildeng", "bamboo-agent-sidekick", false)
                     //copies data from /buildeng-data to /buildeng + the /buildeng/kubernetes flag file
                     .withCommand("sh", "-c", "/kubernetes.sh")
                 .endContainer()
                 .addNewContainer()
-                    .withName("bamboo-agent")
+                    .withName(CONTAINER_NAME_BAMBOOAGENT)
                     .withImage(c.getDockerImage())
 //                    .withImagePullPolicy("Never")
                     .withWorkingDir("/buildeng")
@@ -121,6 +145,17 @@ public class KubernetesIsolatedDockerImpl implements IsolatedAgentService {
                 
                         
         return pb.build();
+    }
+
+    @Override
+    public void onStart() {
+        Map<String, Object> config = new HashMap<>();
+        pluginScheduler.scheduleJob(PLUGIN_JOB_KEY, KubernetesWatchdog.class, config, new Date(), PLUGIN_JOB_INTERVAL_MILLIS);
+    }
+
+    @Override
+    public void onStop() {
+        pluginScheduler.unscheduleJob(PLUGIN_JOB_KEY);
     }
     
 }
---
+++ bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesWatchdog.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 Atlassian.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atlassian.buildeng.kubernetes;
+
+import com.atlassian.sal.api.scheduling.PluginJob;
+import io.fabric8.kubernetes.api.model.ContainerStatus;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author mkleint
+ */
+public class KubernetesWatchdog implements PluginJob {
+
+    @Override
+    public void execute(Map<String, Object> jobDataMap) {
+        try (KubernetesClient client = KubernetesIsolatedDockerImpl.createKubernetesClient()) {
+            //TODO do we want to repeatedly query or 'watch' for changes?
+            //client.pods().watch();
+            
+            List<Pod> pods = client.pods().inNamespace(KubernetesIsolatedDockerImpl.NAMESPACE_BAMBOO).list().getItems();
+            List<Pod> agentDied = pods.stream().filter(new Predicate<Pod>() {
+                @Override
+                public boolean test(Pod t) {
+                    return t.getStatus().getContainerStatuses().stream()
+                            .filter((ContainerStatus t1) -> KubernetesIsolatedDockerImpl.CONTAINER_NAME_BAMBOOAGENT.equals(t1.getName()))
+                            .filter((ContainerStatus t1) -> t1.getState().getTerminated() != null).findFirst().isPresent();
+                }
+            }).collect(Collectors.toList());
+            if (!agentDied.isEmpty()) {
+                System.out.println("agents died, remove pods");
+                client.pods().delete(agentDied);
+                //TODO if job still queued, remove from bamboo queue. + add error
+            }
+            
+        }
+    }
+    
+}
