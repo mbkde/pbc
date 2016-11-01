fc3baa074f9bbdac7034c7af142b6a6eb303678c
Milos Kleint <mkleint@atlassian.com>
2016-11-02 09:09:32 +1100

    BUILDENG-11977 slim down heartbeat

--- ecs-scheduler-service/src/main/java/com/atlassian/buildeng/ecs/api/Heartbeat.java
+++ ecs-scheduler-service/src/main/java/com/atlassian/buildeng/ecs/api/Heartbeat.java
@@ -2,22 +2,11 @@ package com.atlassian.buildeng.ecs.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
-/**
- * Created by ojongerius on 09/05/2016.
- */
 public class Heartbeat {
     private final String status = "ok";
 
-    public  Heartbeat() {
-        // For Jackson
-    }
-
-    public String getHeartbeat() {
-        // System.out.println("Running heartbeat");
+    @JsonProperty
+    public String getStatus() {
         return status;
     }
-
-    @JsonProperty
-    public String getStatus() { return status; }
 }
