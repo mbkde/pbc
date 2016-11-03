fa2e75093240738c297c79c181bff882f574b8dc
Milos Kleint <mkleint@atlassian.com>
2016-11-04 10:55:57 +1100

    BUILDENG-11977 remove unused code and generated comments

--- ecs-scheduler-service/src/main/java/com/atlassian/buildeng/ecs/resources/HeartBeatResource.java
+++ ecs-scheduler-service/src/main/java/com/atlassian/buildeng/ecs/resources/HeartBeatResource.java
@@ -7,10 +7,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-/**
- * Created by ojongerius on 09/05/2016.
- */
-
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/healthcheck")
 public class HeartBeatResource {
