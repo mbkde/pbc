124b38c5c0deb9f4b1045e0d1474c9486493cb4f
Oswyn Brent <obrent@atlassian.com>
2017-05-08 19:52:21 +1000

    BUILDENG-13095: Add precise memory values for i3s

--- ecs-scheduler/src/main/java/com/atlassian/buildeng/ecs/scheduling/ECSInstance.java
+++ ecs-scheduler/src/main/java/com/atlassian/buildeng/ecs/scheduling/ECSInstance.java
@@ -20,8 +20,8 @@ public enum ECSInstance {
     M4_4XLARGE(16384, 64419, "m4.4xlarge"),
     M4_10XLARGE(40960, 161186, "m4.10xlarge"),
     M4_16XLARGE(65536, 257955, "m4.16xlarge"),
-    I3_8XLARGE(32768, 245000, "i3.8xlarge"), // Memory value is not final
-    I3_16XLARGE(65536, 490000,"i3.16xlarge"); //Memory value is not final
+    I3_8XLARGE(32768, 245731, "i3.8xlarge"),
+    I3_16XLARGE(65536, 491683,"i3.16xlarge");
 
     private int cpu;
     private int memory;
