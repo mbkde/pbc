dd65290f0a454481722f72b5d525eae515097892
Milos Kleint <mkleint@atlassian.com>
2017-07-21 11:36:05 +1000

    BUILDENG-13451 calculate the success/fail ratio and avg times of longest job in the same stage. Use those to decide if we perform future reservation or not.

--- bamboo-isolated-docker-plugin/src/main/java/com/atlassian/buildeng/isolated/docker/lifecycle/ReserveFutureCapacityPreJobAction.java
+++ bamboo-isolated-docker-plugin/src/main/java/com/atlassian/buildeng/isolated/docker/lifecycle/ReserveFutureCapacityPreJobAction.java
@@ -16,33 +16,48 @@
 
 package com.atlassian.buildeng.isolated.docker.lifecycle;
 
+import com.atlassian.bamboo.builder.BuildState;
 import com.atlassian.bamboo.chains.BuildExecution;
 import com.atlassian.bamboo.chains.ChainExecution;
 import com.atlassian.bamboo.chains.StageExecution;
 import com.atlassian.bamboo.chains.plugins.PreJobAction;
+import com.atlassian.bamboo.plan.PlanKey;
+import com.atlassian.bamboo.plan.cache.CachedPlanManager;
+import com.atlassian.bamboo.plan.cache.ImmutablePlan;
+import com.atlassian.bamboo.resultsummary.ResultsSummary;
+import com.atlassian.bamboo.resultsummary.ResultsSummaryCriteria;
+import com.atlassian.bamboo.resultsummary.ResultsSummaryManager;
 import com.atlassian.bamboo.v2.build.BuildContext;
 import com.atlassian.buildeng.spi.isolated.docker.AccessConfiguration;
 import com.atlassian.buildeng.spi.isolated.docker.Configuration;
 import com.atlassian.buildeng.spi.isolated.docker.IsolatedAgentService;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReserveFutureCapacityPreJobAction implements PreJobAction {
+
+    private static final int MINIMUM_SUCCESS_PERCENTAGE = 50;
+    private static final int MINIMUM_RESULT_COUNT = 5;
+    private static final int TOTAL_RESULT_COUNT = 10;
+
     private final static Logger logger = LoggerFactory.getLogger(ReserveFutureCapacityPreJobAction.class);
     private final IsolatedAgentService isoService;
+    private final ResultsSummaryManager resultsSummaryManager;
+    private final CachedPlanManager cachedPlanManager;
 
-    public ReserveFutureCapacityPreJobAction(IsolatedAgentService isoService) {
+    public ReserveFutureCapacityPreJobAction(IsolatedAgentService isoService, ResultsSummaryManager resultsSummaryManager, CachedPlanManager cachedPlanManager) {
         this.isoService = isoService;
+        this.resultsSummaryManager = resultsSummaryManager;
+        this.cachedPlanManager = cachedPlanManager;
     }
 
     @Override
     public void execute(StageExecution stageExecution, BuildContext buildContext) {
-        //set the buildKey in customData because we are not having a way to retrieve it in postJobAction otherwise
-        buildContext.getBuildResult().getCustomBuildData().put(PostJobActionImpl.PBCBUILD_KEY, buildContext.getBuildKey().getKey());
-
         Long currentStageMem = stagePBCExecutions(stageExecution.getChainExecution(), stageExecution.getStageIndex()).collect(Collectors.summingLong((Configuration value) -> value.getMemoryTotal()));
         Long currentStageCpu = stagePBCExecutions(stageExecution.getChainExecution(), stageExecution.getStageIndex()).collect(Collectors.summingLong((Configuration value) -> value.getCPUTotal()));
         Long nextStageMem = stagePBCExecutions(stageExecution.getChainExecution(), stageExecution.getStageIndex() + 1).collect(Collectors.summingLong((Configuration value) -> value.getMemoryTotal()));
@@ -50,13 +65,18 @@ public class ReserveFutureCapacityPreJobAction implements PreJobAction {
         long diffCpu = Math.max(0, nextStageCpu - currentStageCpu);
         long diffMem = Math.max(0, nextStageMem - currentStageMem);
         if (diffMem > 0 || diffCpu > 0) {
-            logger.info("Adding future reservation for " + buildContext.getBuildKey().getKey() + " " + buildContext.getPlanResultKey());
-            isoService.reserveCapacity(buildContext.getBuildKey(), 
+            long seconds = stageSuccessfulCompletionAvg(stageExecution);
+            if (seconds != -1) { //for now ignore the time it takes, only care about succ/fail ratio
+                //set the buildKey in customData because we are not having a way to retrieve it in postJobAction otherwise
+                buildContext.getBuildResult().getCustomBuildData().put(PostJobActionImpl.FUTURE_RESERVE_PBCBUILD_KEY, buildContext.getBuildKey().getKey());
+                logger.info("Adding future reservation for " + buildContext.getBuildKey().getKey() + " " + buildContext.getPlanResultKey());
+                //TODO we should coalesce multiple calls for buildKey+resultKeys into single call.
+                isoService.reserveCapacity(buildContext.getBuildKey(),
                     stagePBCJobResultKeys(stageExecution.getChainExecution(), stageExecution.getStageIndex() + 1),
                     diffMem, diffCpu);
+            }
         }
     }
-
 
     static Stream<Configuration> stagePBCExecutions(ChainExecution chainExecution, int stageIndex) {
         Stream<Configuration> stream = chainExecution.getStages().stream()
@@ -67,12 +87,72 @@ public class ReserveFutureCapacityPreJobAction implements PreJobAction {
         return stream;
     }
 
-    static  List<String> stagePBCJobResultKeys(ChainExecution chainExecution, int stageIndex) {
+    static List<String> stagePBCJobResultKeys(ChainExecution chainExecution, int stageIndex) {
         return chainExecution.getStages().stream()
                 .filter((StageExecution t) -> t.getStageIndex() == stageIndex)
                 .flatMap((StageExecution t) -> t.getBuilds().stream())
                 .filter((BuildExecution t) -> AccessConfiguration.forContext(t.getBuildContext()).isEnabled())
                 .map((BuildExecution t) -> t.getBuildContext().getResultKey().getKey())
                 .collect(Collectors.toList());
+    }
+
+    long stageSuccessfulCompletionAvg(StageExecution stageExecution) {
+        // for each job in stage, try to collect 10 build results in current branch, if not
+        // present fallback to master branch.
+        // once we have these, for each job's results calculate the success ratio and avg time it takes to build.
+        // reduce to get the minumum success rate and maximum avg time across jobs.
+        Optional<Stats> stats = stageExecution.getBuilds().stream()
+                .map((BuildExecution t) -> t.getPlanResultKey().getPlanKey())
+                .map((PlanKey t) -> cachedPlanManager.getPlanByKey(t))
+                .map((ImmutablePlan t) -> {
+                    final ResultsSummaryCriteria criteria = new ResultsSummaryCriteria(t.getPlanKey().getKey());
+                    criteria.setMaxRowCount(TOTAL_RESULT_COUNT);
+                    List<ResultsSummary> summs = new ArrayList<>();
+                    summs.addAll(resultsSummaryManager.getResultSummaries(criteria));
+                    if (summs.size() < TOTAL_RESULT_COUNT && t.getMaster() != null) {
+                        final ResultsSummaryCriteria masterCrit = new ResultsSummaryCriteria(t.getMaster().getPlanKey().getKey());
+                        masterCrit.setMaxRowCount(TOTAL_RESULT_COUNT - summs.size());
+                        summs.addAll(resultsSummaryManager.getResultSummaries(masterCrit));
+                    }
+                    return summs;
+                })
+                .map((List<ResultsSummary> t) -> {
+                    int count = t.size();
+                    long sum = 0;
+                    int succCount = 0;
+                    for (ResultsSummary s : t) {
+                        if (BuildState.SUCCESS.equals(s.getBuildState())) {
+                            succCount = succCount + 1;
+                            sum = sum + s.getDuration();
+                        }
+                    }
+                    return new Stats(count, count != 0 ? (succCount * 100) / count : 0 , succCount != 0 ? sum / succCount : 0);
+                })
+                .reduce((Stats t, Stats u) -> new Stats(Math.min(t.count, u.count), Math.min(t.successPercentage, u.successPercentage), Math.max(t.avgDuration, u.avgDuration)));
+
+        logger.debug("TEST_SECONDS:" + stageExecution.getChainExecution().getPlanResultKey() + "  " + stats.toString());
+        //compare collected data to minimum requirements
+        if (stats.isPresent() && stats.get().count > MINIMUM_RESULT_COUNT && stats.get().successPercentage > MINIMUM_SUCCESS_PERCENTAGE) {
+            return stats.get().avgDuration;
+        } else {
+            return -1;
+        }
+    }
+
+    private class Stats {
+        final int count;
+        final int successPercentage;
+        final long avgDuration;
+
+        public Stats(int count, int successPerc, long avgDuration) {
+            this.count = count;
+            this.successPercentage = successPerc;
+            this.avgDuration = avgDuration;
+        }
+
+        @Override
+        public String toString() {
+            return "Stats{" + "count=" + count + ", successPercentage=" + successPercentage + ", avgDuration=" + avgDuration + '}';
+        }
     }
 }
---
+++ bamboo-isolated-docker-plugin/src/test/java/com/atlassian/buildeng/isolated/docker/lifecycle/ReserveFutureCapacityPreJobActionTest.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2017 Atlassian Pty Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atlassian.buildeng.isolated.docker.lifecycle;
+
+import com.atlassian.bamboo.builder.BuildState;
+import com.atlassian.bamboo.chains.BuildExecution;
+import com.atlassian.bamboo.chains.ChainExecution;
+import com.atlassian.bamboo.chains.StageExecution;
+import com.atlassian.bamboo.plan.PlanKeys;
+import com.atlassian.bamboo.plan.PlanResultKey;
+import com.atlassian.bamboo.plan.cache.CachedPlanManager;
+import com.atlassian.bamboo.plan.cache.ImmutablePlan;
+import com.atlassian.bamboo.resultsummary.ResultsSummary;
+import com.atlassian.bamboo.resultsummary.ResultsSummaryCriteria;
+import com.atlassian.bamboo.resultsummary.ResultsSummaryManager;
+import com.atlassian.buildeng.spi.isolated.docker.IsolatedAgentService;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReserveFutureCapacityPreJobActionTest {
+    @Mock
+    private IsolatedAgentService isoService;
+    @Mock
+    private ResultsSummaryManager resultsSummaryManager;
+    @Mock
+    private CachedPlanManager cachedPlanManager;
+
+    @InjectMocks
+    private ReserveFutureCapacityPreJobAction action;
+
+    @Test
+    public void testNoResults() {
+        StageExecution stage = mockStageExecution(new String[] { "AAA-BBB-CCC-22"});
+        when(resultsSummaryManager.getResultSummaries(Matchers.eq(new ResultsSummaryCriteria(PlanKeys.getPlanKey("AAA-BBB-CCC").getKey())))).thenReturn(Collections.emptyList());
+        long ret = action.stageSuccessfulCompletionAvg(stage);
+        Assert.assertEquals(-1, ret);
+    }
+
+    @Test
+    public void testNotEnoughResults() {
+        StageExecution stage = mockStageExecution(new String[] { "AAA-BBB-CCC-22"});
+        final List<ResultsSummary> summaries = summaries(new Summ[] {
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(500, BuildState.UNKNOWN),
+            new Summ(500, BuildState.FAILED)
+        });
+        when(resultsSummaryManager.getResultSummaries(Matchers.eq(new ResultsSummaryCriteria(PlanKeys.getPlanKey("AAA-BBB-CCC").getKey()))))
+                .thenReturn(summaries);
+        long ret = action.stageSuccessfulCompletionAvg(stage);
+        Assert.assertEquals(-1, ret);
+    }
+
+    @Test
+    public void testNotEnoughSuccessfulResults() {
+        StageExecution stage = mockStageExecution(new String[] { "AAA-BBB-CCC-22"});
+        final List<ResultsSummary> summaries = summaries(new Summ[] {
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(500, BuildState.UNKNOWN),
+            new Summ(500, BuildState.FAILED),
+            new Summ(500, BuildState.FAILED),
+            new Summ(500, BuildState.FAILED),
+            new Summ(500, BuildState.FAILED),
+            new Summ(500, BuildState.FAILED)
+        });
+        when(resultsSummaryManager.getResultSummaries(Matchers.eq(new ResultsSummaryCriteria(PlanKeys.getPlanKey("AAA-BBB-CCC").getKey()))))
+                .thenReturn(summaries);
+        long ret = action.stageSuccessfulCompletionAvg(stage);
+        Assert.assertEquals(-1, ret);
+    }
+
+    @Test
+    public void testSuccessfulResults() {
+        StageExecution stage = mockStageExecution(new String[] { "AAA-BBB-CCC-22"});
+        final List<ResultsSummary> summaries = summaries(new Summ[] {
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(500, BuildState.UNKNOWN),
+            new Summ(500, BuildState.FAILED),
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(1000, BuildState.SUCCESS),
+        });
+        when(resultsSummaryManager.getResultSummaries(Matchers.eq(new ResultsSummaryCriteria(PlanKeys.getPlanKey("AAA-BBB-CCC").getKey()))))
+                .thenReturn(summaries);
+        long ret = action.stageSuccessfulCompletionAvg(stage);
+        Assert.assertEquals(1000, ret);
+    }
+
+    @Test
+    public void testSuccessfulResultsMoreJobs() {
+        StageExecution stage = mockStageExecution(new String[] { "AAA-BBB-CCC-22", "AAA-BBB-DDD-22"});
+        final List<ResultsSummary> summaries = summaries(new Summ[] {
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(500, BuildState.UNKNOWN),
+            new Summ(500, BuildState.FAILED),
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(1000, BuildState.SUCCESS),
+        });
+        when(resultsSummaryManager.getResultSummaries(Matchers.eq(new ResultsSummaryCriteria(PlanKeys.getPlanKey("AAA-BBB-CCC").getKey()))))
+                .thenReturn(summaries);
+        final List<ResultsSummary> summaries2 = summaries(new Summ[] {
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+        });
+        when(resultsSummaryManager.getResultSummaries(Matchers.eq(new ResultsSummaryCriteria(PlanKeys.getPlanKey("AAA-BBB-DDD").getKey()))))
+                .thenReturn(summaries2);
+
+        long ret = action.stageSuccessfulCompletionAvg(stage);
+        Assert.assertEquals(2000, ret);
+    }
+
+    @Test
+    public void testFailedResultsMoreJobs() {
+        StageExecution stage = mockStageExecution(new String[] { "AAA-BBB-CCC-22", "AAA-BBB-DDD-22"});
+        final List<ResultsSummary> summaries = summaries(new Summ[] {
+            new Summ(1000, BuildState.SUCCESS),
+            new Summ(500, BuildState.UNKNOWN),
+            new Summ(500, BuildState.FAILED),
+            new Summ(500, BuildState.FAILED),
+            new Summ(500, BuildState.FAILED),
+            new Summ(500, BuildState.FAILED),
+            new Summ(500, BuildState.FAILED),
+        });
+        when(resultsSummaryManager.getResultSummaries(Matchers.eq(new ResultsSummaryCriteria(PlanKeys.getPlanKey("AAA-BBB-CCC").getKey()))))
+                .thenReturn(summaries);
+        final List<ResultsSummary> summaries2 = summaries(new Summ[] {
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+            new Summ(2000, BuildState.SUCCESS),
+        });
+        when(resultsSummaryManager.getResultSummaries(Matchers.eq(new ResultsSummaryCriteria(PlanKeys.getPlanKey("AAA-BBB-DDD").getKey()))))
+                .thenReturn(summaries2);
+
+        long ret = action.stageSuccessfulCompletionAvg(stage);
+        Assert.assertEquals(-1, ret);
+    }
+
+
+    private List<ResultsSummary> summaries(Summ[] summaries) {
+        List<ResultsSummary> toRet = new ArrayList<>();
+        for (Summ s : summaries) {
+            ResultsSummary rs = mock(ResultsSummary.class);
+            when(rs.getBuildState()).thenReturn(s.buildState);
+            when(rs.getDuration()).thenReturn(s.duration);
+            toRet.add(rs);
+        }
+        return toRet;
+    }
+
+    private StageExecution mockStageExecution(String[] par) {
+        StageExecution stage = mock(StageExecution.class);
+        final List<BuildExecution> collect = Stream.of(par)
+                .map((String t) -> PlanKeys.getPlanResultKey(t))
+                .map((PlanResultKey t) -> {
+                    BuildExecution e = mock(BuildExecution.class);
+                    when(e.getPlanResultKey()).thenReturn(t);
+                    ImmutablePlan plan = mock(ImmutablePlan.class);
+                    when(plan.getPlanKey()).thenReturn(t.getPlanKey());
+                    when(cachedPlanManager.getPlanByKey(Matchers.eq(t.getPlanKey()))).thenReturn(plan);
+                    return e;
+                }).collect(Collectors.toList());
+        when(stage.getBuilds()).thenReturn(collect);
+        when(stage.getChainExecution()).thenReturn(mock(ChainExecution.class));
+
+        return stage;
+    }
+
+    private static class Summ {
+        final long duration;
+        final BuildState buildState;
+
+        public Summ(long duration, BuildState buildState) {
+            this.duration = duration;
+            this.buildState = buildState;
+        }
+    }
+
+}
