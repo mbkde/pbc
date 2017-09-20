3bcf79fc6ed9e6eb65a2841acae7b772966f1f1c
Milos Kleint <mkleint@atlassian.com>
2017-09-20 09:49:13 +0200

    BUILDENG-13829 just checkstyle

--- bamboo-kubernetes-backend-plugin/src/test/java/com/atlassian/buildeng/kubernetes/KubernetesIsolatedDockerImplTest.java
+++ bamboo-kubernetes-backend-plugin/src/test/java/com/atlassian/buildeng/kubernetes/KubernetesIsolatedDockerImplTest.java
@@ -16,54 +16,53 @@
 
 package com.atlassian.buildeng.kubernetes;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
-import org.junit.Test;
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.SafeConstructor;
-
 import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
 
 public class KubernetesIsolatedDockerImplTest {
     @Test
     @SuppressWarnings("unchecked")
     public void testContainersMergedByName() {
         Yaml yaml =  new Yaml(new SafeConstructor());
-        String ts = "apiVersion: v1\n" +
-                "kind: Pod\n" +
-                "metadata:\n" +
-                "  name: aws-cli\n" +
-                "spec:\n" +
-                "  containers:\n" +
-                "    - name: main\n" +
-                "      volumeMounts:\n" +
-                "          - name: secrets\n" +
-                "            mountPath: /root/.aws\n" +
-                "    - name: myContainer2\n" +
-                "      image: xueshanf/awscli:latest\n" +
-                "  restartPolicy: Never\n" +
-                "  volumes:\n" +
-                "    - name: secrets\n" +
-                "      secret:\n" +
-                "        secretName: bitbucket-bamboo\n";
+        String ts = "apiVersion: v1\n"
+              + "kind: Pod\n"
+              + "metadata:\n"
+              + "  name: aws-cli\n"
+              + "spec:\n"
+                
+              + "  containers:\n"
+              + "    - name: main\n"
+              + "      volumeMounts:\n"
+              + "          - name: secrets\n"
+              + "            mountPath: /root/.aws\n"
+              + "    - name: myContainer2\n"
+              + "      image: xueshanf/awscli:latest\n"
+              + "  restartPolicy: Never\n"
+              + "  volumes:\n"
+              + "    - name: secrets\n"
+              + "      secret:\n"
+              + "        secretName: bitbucket-bamboo\n";
         String os =
-                "metadata:\n" +
-                "    namespace: buildeng\n" +
-                "    annotations:\n" +
-                "        iam.amazonaws.com/role: arn:aws:iam::123456678912:role/staging-bamboo\n" +
-                "spec:\n" +
-                "  containers:\n" +
-                "    - name: main\n" +
-                "      image: xueshanf/awscli:latest\n" +
-                "    - name: myContainer3\n" +
-                "      image: xueshanf/awscli:latest\n" +
-                "  volumes:\n" +
-                "    - name: myvolume\n";
+                "metadata:\n"
+              + "    namespace: buildeng\n"
+              + "    annotations:\n"
+              + "        iam.amazonaws.com/role: arn:aws:iam::123456678912:role/staging-bamboo\n"
+              + "spec:\n"
+              + "  containers:\n"
+              + "    - name: main\n"
+              + "      image: xueshanf/awscli:latest\n"
+              + "    - name: myContainer3\n"
+              + "      image: xueshanf/awscli:latest\n"
+              + "  volumes:\n"
+              + "    - name: myvolume\n";
 
         Map<String, Object> template = (Map<String, Object>) yaml.load(ts);
         Map<String, Object> overrides = (Map<String, Object>) yaml.load(os);
