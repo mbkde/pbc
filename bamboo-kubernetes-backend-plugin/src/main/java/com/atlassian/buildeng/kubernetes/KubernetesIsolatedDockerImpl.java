ef1de59b76e37d32849cdb4182a6f115b84784aa
Milos Kleint <mkleint@atlassian.com>
2018-01-25 12:12:39 +1100

    noissue: add kube pod name to the log message to be able to align the message with others

--- bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesIsolatedDockerImpl.java
+++ bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesIsolatedDockerImpl.java
@@ -100,9 +100,10 @@ public class KubernetesIsolatedDockerImpl implements IsolatedAgentService, Lifec
             File podFile = createPodFile(finalPod);
             Pod pod = new KubernetesClient(globalConfiguration).createPod(podFile);
             Duration servedIn = Duration.ofMillis(System.currentTimeMillis() - request.getQueueTimestamp());
-            logger.info("Kubernetes successfully processed request for {} in {}", request.getResultKey(), servedIn);
+            String name = KubernetesHelper.getName(pod);
+            logger.info("Kubernetes successfully processed request for {} in {}, pod name: {}", request.getResultKey(), servedIn, name);
             callback.handle(new IsolatedDockerAgentResult()
-                    .withCustomResultData(NAME, KubernetesHelper.getName(pod))
+                    .withCustomResultData(NAME, name)
                     .withCustomResultData(UID, pod.getMetadata().getUid()));
 
         } catch (KubernetesClient.KubectlException e) {
