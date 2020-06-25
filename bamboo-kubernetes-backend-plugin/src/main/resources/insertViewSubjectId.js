ebda0f1c1bfa53c9d11cb83494b49359c8ee2a6d
Raymond Chow <rchow@atlassian.com>
2020-06-25 16:56:06 +1000

    BUILDENG-18363: Fixed permissions so that users with only view permissions can obtain subject ID through UI. Added new button to accomodate this.

--- bamboo-kubernetes-backend-plugin/src/main/resources/insertViewSubjectId.js
+++ bamboo-kubernetes-backend-plugin/src/main/resources/insertViewSubjectId.js
@@ -1,17 +1,38 @@
-(function($) {
-    $(document).ready(function() {
-        if($('#project-configuration-actions').length) {
-            var searchStr = window.location.search;
-            var deploymentId = searchStr.split("=")[1];
+(function ($) {
+    $(document).ready(function () {
+        const searchStr = new URLSearchParams(window.location.search);
+        const deploymentId = searchStr.get("id");
+        const hrefPath = AJS.contextPath() + "/rest/pbc-kubernetes/1.0/subjectIdForDeployment/" + deploymentId;
 
-            var element = $('#create-deployment-version').get(0);
-            var content = $(
-                '<li><a class="aui-icon-container" href="' + AJS.contextPath()
-                + '/rest/pbc-kubernetes/1.0/subjectIdForDeployment/' + deploymentId
-                + '">View AWS IAM Subject ID for PBC</a></li>'
+        if (
+            $("#project-configuration-actions").length ||
+            $("#deployment-configuration-actions").length ||
+            $("#environment-configuration-actions").length
+        ) {
+            // "Configure deployment project" screen has "Create release" button in a separate section to the rest
+            // of the items. Grab something lower down on the list so the new button appears at the bottom.
+            // Both are still needed, as "Deploy" permission only grants the "Create release button" elsewhere in the UI
+            const menuElement = $("#edit-deployment-project").get(0) || $("#create-deployment-version").get(0);
+            const content = $(
+                '<li><a class="aui-icon-container" href="' +
+                hrefPath +
+                '">View AWS IAM Subject ID for PBC</a></li>'
             );
 
-            content.appendTo(element.parentNode.parentNode);
+            content.appendTo(menuElement.parentNode.parentNode);
+        } else {
+            const innerHeaderElement = document.querySelector("#content > header > div");
+
+            // Need absolute position if no buttons since the header won't expand to full size otherwise
+            const content = $(
+                '<a id="subject-id-button" class="aui-buttons aui-button" ' +
+                'style="right: 10px; position: absolute; font-size: 14px; top: 30px; margin-left: 20px;" ' +
+                'href="' +
+                hrefPath +
+                '">View AWS IAM Subject ID for PBC</a>'
+            );
+
+            content.appendTo(innerHeaderElement);
         }
     });
 })(AJS.$);
