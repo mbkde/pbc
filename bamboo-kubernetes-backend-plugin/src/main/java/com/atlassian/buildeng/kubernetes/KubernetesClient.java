a083fbb9d663d2691a547393484dfa05d6809e2b
Ailin Zhang <azhang2@atlassian.com>
2021-07-07 14:46:39 +1000

    BUILDENG-19298: add --wait=false to pod deletion

--- bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesClient.java
+++ bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesClient.java
@@ -201,7 +201,7 @@ public class KubernetesClient {
             throws KubectlException {
         long startTime = System.currentTimeMillis();
         executeKubectl(new PodContextSupplier(pod),
-                "delete", "pod", "--grace-period=0 --force", "--timeout=" + Constants.KUBECTL_DELETE_TIMEOUT,
+                "delete", "pod", "--grace-period=0 --force --wait=false", "--timeout=" + Constants.KUBECTL_DELETE_TIMEOUT,
                 KubernetesHelper.getName(pod));
         long podDeletionEnd = System.currentTimeMillis();
         deletePodLogger.log(String.format("pod deletion took %f ms", podDeletionEnd - startTime));
