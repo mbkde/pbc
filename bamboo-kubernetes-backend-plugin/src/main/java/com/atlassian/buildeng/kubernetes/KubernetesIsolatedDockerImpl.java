a058ac63d22ffeb83aeefef268db415c7befa92d
Michael Walsh <mwalsh2@atlassian.com>
2022-07-14 16:38:39 +1200

    Additional removal that I missed last time

--- bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesIsolatedDockerImpl.java
+++ bamboo-kubernetes-backend-plugin/src/main/java/com/atlassian/buildeng/kubernetes/KubernetesIsolatedDockerImpl.java
@@ -220,7 +220,6 @@ public class KubernetesIsolatedDockerImpl implements IsolatedAgentService, Lifec
                         + " plugin are not still running.");
         List<JobKey> previousJobKeys = Arrays.asList(PLUGIN_JOB_KEY, PLUGIN_JOB_JMX_KEY);
         schedulerUtils.awaitPreviousJobExecutions(previousJobKeys);
-        schedulerUtils.deleteJobs(previousJobKeys);
 
         JobDataMap config = new JobDataMap();
         config.put("globalConfiguration", globalConfiguration);
