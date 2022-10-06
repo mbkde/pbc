2cd54ee8e95d99e2e1743e78dae69c5c4ebe2779
Jan Majkutewicz <jmajkutewicz@atlassian.com>
2022-10-06 21:37:37 +0200

    BDEV-16966 basic pbc integration test

--- bamboo-kubernetes-backend-plugin/src/test/java/com/atlassian/buildeng/kubernetes/pageobject/PerBuildContainerConfigPage.java
+++ bamboo-kubernetes-backend-plugin/src/test/java/com/atlassian/buildeng/kubernetes/pageobject/PerBuildContainerConfigPage.java
@@ -18,14 +18,18 @@ package com.atlassian.buildeng.kubernetes.pageobject;
 
 import static com.atlassian.pageobjects.elements.query.Conditions.forSupplier;
 import static com.atlassian.pageobjects.elements.query.Poller.waitUntilTrue;
+import static org.junit.Assert.assertTrue;
 
 import com.atlassian.aui.auipageobjects.AuiCheckbox;
 import com.atlassian.bamboo.pageobjects.pages.plan.configuration.ConfigureJobDockerPage;
 import com.atlassian.bamboo.plan.PlanKey;
 import com.atlassian.pageobjects.elements.ElementBy;
+import com.atlassian.pageobjects.elements.Option;
 import com.atlassian.pageobjects.elements.PageElement;
 import com.atlassian.pageobjects.elements.PageElementFinder;
+import com.atlassian.pageobjects.elements.SelectElement;
 import com.atlassian.pageobjects.elements.timeout.TimeoutType;
+import java.util.Optional;
 import javax.inject.Inject;
 import org.openqa.selenium.By;
 
@@ -34,9 +38,11 @@ public class PerBuildContainerConfigPage extends ConfigureJobDockerPage {
     @ElementBy(id = "isolationTypePBC")
     private AuiCheckbox choosePbc;
     @ElementBy(id = "configureDocker_custom_isolated_docker_image")
-    private PageElement dockerImage;
+    private PageElement dockerImageInput;
     @ElementBy(id = ISOLATED_DOCKER_AWS_ROLE)
     private PageElement awsIamRole;
+    @ElementBy(id = "configureDocker_custom_isolated_docker_imageSize")
+    private SelectElement agentSizeSelect;
     @Inject
     private PageElementFinder pageElementFinder;
 
@@ -45,11 +51,11 @@ public class PerBuildContainerConfigPage extends ConfigureJobDockerPage {
     }
 
     public boolean isDockerImageVisible() {
-        return dockerImage.isVisible();
+        return dockerImageInput.isVisible();
     }
 
     public boolean isDockerImagePresent() {
-        return dockerImage.isPresent();
+        return dockerImageInput.isPresent();
     }
 
     public boolean eitherDockerImageVisibleOrWarning() {
@@ -61,8 +67,25 @@ public class PerBuildContainerConfigPage extends ConfigureJobDockerPage {
         return !pageElementFinder.findAll(By.id(ISOLATED_DOCKER_AWS_ROLE)).isEmpty();
     }
 
-    public void choosePerBuildContainerPlugin() {
+    public PerBuildContainerConfigPage choosePerBuildContainerPlugin() {
         choosePbc.click();
         waitUntilTrue(forSupplier(timeouts.timeoutFor(TimeoutType.SLOW_PAGE_LOAD), this::eitherDockerImageVisibleOrWarning));
+        return this;
+    }
+
+    public PerBuildContainerConfigPage setDockerImage(final String dockerImage) {
+        dockerImageInput.clear().type(dockerImage);
+        return this;
+    }
+
+    public PerBuildContainerConfigPage selectAgentSize(final String sizeName) {
+        Optional<Option> agentSizeOption =
+                agentSizeSelect.getAllOptions().stream()
+                        .filter(size -> size.text().startsWith(sizeName))
+                        .findFirst();
+        assertTrue(agentSizeOption.isPresent());
+        agentSizeSelect.select(agentSizeOption.get());
+
+        return this;
     }
 }
---
+++ bamboo-kubernetes-backend-plugin/src/test/java/it/com/atlassian/buildeng/kubernetes/PbcSetupTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2022 Atlassian Pty Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.com.atlassian.buildeng.kubernetes;
+
+import com.atlassian.bamboo.plan.PlanKey;
+import com.atlassian.bamboo.plan.PlanKeys;
+import com.atlassian.bamboo.plan.PlanResultKey;
+import com.atlassian.bamboo.specs.api.builders.plan.Job;
+import com.atlassian.bamboo.specs.api.builders.plan.Plan;
+import com.atlassian.bamboo.specs.api.model.plan.PlanProperties;
+import com.atlassian.bamboo.specs.builders.task.ScriptTask;
+import com.atlassian.bamboo.testutils.backdoor.model.queue.RestQueuedBuild;
+import com.atlassian.bamboo.testutils.specs.TestPlanSpecsHelper;
+import com.atlassian.bamboo.testutils.user.TestUser;
+import com.atlassian.buildeng.kubernetes.pageobject.GenericKubernetesConfigPage;
+import com.atlassian.buildeng.kubernetes.pageobject.GenericPerBuildContainerConfigurationPage;
+import com.atlassian.buildeng.kubernetes.pageobject.PerBuildContainerConfigPage;
+import com.atlassian.pageobjects.elements.query.Poller;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+public class PbcSetupTest extends AbstractPbcTest {
+
+    /**
+     * Currently we used a hacked sidekick image that can update the agent during startup
+     * The image can be found on branch: BDEV-16966-sidekick-for-integration-tests
+     * <p>
+     * Should be removed/replaced when we decide what to do with sidekick in PBC
+     */
+    private static final String SIDEKICK_IMAGE = "docker.atl-paas.net/bamboo/bamboo-agent-sidekick:integration-tests";
+
+    /**
+     * Use longer timeout for build, so a PBC agent have enough time to start
+     */
+    private static final Poller.WaitTimeout PBC_BUILD_WAIT_TIMEOUT = Poller.by(10, TimeUnit.MINUTES);
+
+    @Test
+    public void testPbcConfiguration() throws Exception {
+        //TODO bamboo and broker address
+        bamboo.fastLogin(TestUser.ADMIN);
+
+        backdoor.agents().enableRemoteAgents(true);
+
+        bamboo.visit(GenericPerBuildContainerConfigurationPage.class)
+                .setEnableSwitch(true)
+                .setAgentCreationThrottling(100)
+                .setArchitectureConfig("")
+                .save();
+        bamboo.visit(GenericKubernetesConfigPage.class)
+                .setSidekickImage(SIDEKICK_IMAGE)
+                .setCurrentContext("")
+                .setPodTemplate(getPodTemplateAsString())
+                .setArchitecturePodConfig("")
+                .save();
+
+        // TODO move this to another suite
+        final String pythonImage = "python:3.10-slim-bullseye";
+        bamboo.fastLogin(TestUser.ADMIN);
+
+        final Job pythonJob = TestPlanSpecsHelper.defaultJob();
+        pythonJob.tasks(new ScriptTask().inlineBody("set -eux\npython --version"));
+
+        final Plan plan = TestPlanSpecsHelper.singleJobPlan(pythonJob);
+        final PlanKey planKey = TestPlanSpecsHelper.getPlanKey(plan);
+        final PlanProperties planProperties = backdoor.plans().createPlan(plan);
+
+        final PlanKey defaultJobKey = TestPlanSpecsHelper.getDefaultJobKey(planProperties);
+        bamboo.visit(PerBuildContainerConfigPage.class, defaultJobKey)
+                .choosePerBuildContainerPlugin()
+                .setDockerImage(pythonImage)
+                .selectAgentSize("Extra Small")
+                .saveExpectingSuccess();
+
+        triggerBuildAndAwaitSuccess(planKey);
+    }
+
+    private void triggerBuildAndAwaitSuccess(PlanKey planKey) {
+        final RestQueuedBuild queuedBuildResponse = backdoor.plans().triggerBuildWithResponse(planKey, Collections.emptyMap());
+        final PlanResultKey planResultKey = PlanKeys.getPlanResultKey(queuedBuildResponse.getBuildResultKey());
+        backdoor.plans().waitForSuccessfulBuild(planResultKey, PBC_BUILD_WAIT_TIMEOUT);
+    }
+
+    private String getPodTemplateAsString() throws IOException {
+        return FileUtils.readFileToString(new File(Objects.requireNonNull(this.getClass()
+                .getResource("/basePodTemplate.yaml")).getFile()), "UTF-8");
+    }
+}
